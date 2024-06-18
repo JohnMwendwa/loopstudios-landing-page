9d643d485b9a5200f66a73062e70839999c34af9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-06-18 20:01:21 +0300

    Set date dynamically

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mobileMenuSlider = document.querySelector(".mobile__menu--slider");
 const hamburgerBtn = document.querySelector(".btn__menu--hamburger");
 const closeBtn = document.querySelector(".btn__menu--close");
+const date = document.querySelector(".date");
 
 hamburgerBtn.addEventListener("click", () => {
   mobileMenuSlider.classList.add("active");
@@ -9,3 +10,5 @@ hamburgerBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   mobileMenuSlider.classList.remove("active");
 });
+
+date.textContent = new Date().getFullYear();
